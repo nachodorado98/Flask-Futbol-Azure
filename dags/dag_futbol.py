854edba88dc3419b2059bd6353f5f7a368534d16
nacho dorado <natxo98@gmail.com>
2024-06-13 18:33:41 +0200

    Reorganizacion de tareas en Apache Airflow

--- dags/dag_futbol.py
+++ dags/dag_futbol.py
@@ -2,6 +2,8 @@ from airflow import DAG
 from datetime import datetime
 from airflow.operators.python import PythonOperator, BranchPythonOperator
 from airflow.operators.bash_operator import BashOperator
+from airflow.utils.task_group import TaskGroup
+from airflow.operators.dummy_operator import DummyOperator
 import os
 import time
 
@@ -13,11 +15,19 @@ from python.src.datalake.conexion_data_lake import ConexionDataLake
 
 def existe_entorno()->str:
 
-	return "pipeline_equipos_ligas" if os.path.exists(os.path.join(os.getcwd(), "dags", "entorno")) else "carpeta_logs"
+	return "entorno.entorno_creado" if os.path.exists(os.path.join(os.getcwd(), "dags", "entorno")) else "entorno.carpeta_logs"
+
+def vaciarCarpeta(ruta:str)->None:
+
+	if os.path.exists(ruta):
+
+		for archivo in os.listdir(ruta):
+
+			os.remove(os.path.join(ruta, archivo))
 
 def crearArchivoLog(motivo:str)->None:
 
-	archivo_log=f"log_{datetime.now().strftime('%Y%m%d_%H%M%S')}.txt"
+	archivo_log=f"log_{datetime.now().strftime('%Y%m%d_%H%M%S.%f')}.txt"
 
 	ruta_log=os.path.join(os.getcwd(), "dags", "entorno", "logs", archivo_log)
 
@@ -69,7 +79,7 @@ def Pipeline(funcion):
 
 				crearArchivoLog(mensaje)
 
-			time.sleep(0.5)
+			time.sleep(1)
 
 		con.cerrarConexion()
 
@@ -91,7 +101,35 @@ def Pipeline_Entrenador_Equipos(equipo):
 def Pipeline_Estadio_Equipos(equipo):
 	ETL_Estadio_Equipo(equipo)
 
-def descargarEscudos()->None:
+def data_lake_disponible()->str:
+
+	try:
+
+		con=ConexionDataLake()
+
+		con.cerrarConexion()
+
+		return "datalake.entorno_data_lake_creado"
+
+	except Exception:
+
+		return "log_data_lake"
+
+def entorno_data_lake_creado():
+
+	if not entorno_creado("contenedorequipos"):
+
+		return "datalake.crear_entorno_data_lake"
+
+	return "datalake.no_crear_entorno_data_lake"
+
+def creacion_entorno_data_lake()->None:
+
+	crearEntornoDataLake("contenedorequipos", "escudos")
+
+	print("Entorno Data Lake creado")
+
+def subirEscudosDataLake()->None:
 
 	con=Conexion()
 
@@ -103,7 +141,7 @@ def descargarEscudos()->None:
 
 	for codigo in codigo_escudos:
 
-		print(f"Descargando imagen {codigo}...")
+		print(f"Descargando escudo {codigo}...")
 
 		try:
 
@@ -111,95 +149,82 @@ def descargarEscudos()->None:
 
 		except Exception as e:
 
-			mensaje=f"Imagen: {codigo} - Motivo: {e}"
+			mensaje=f"Escudo: {codigo} - Motivo: {e}"
 
-			print(f"Error en imagen con codigo {codigo}")
+			print(f"Error en escudo con codigo {codigo}")
 
 			crearArchivoLog(mensaje)
 
 	print("Descarga de escudos finalizada")
 
-def data_lake_disponible()->str:
-
 	try:
 
-		con=ConexionDataLake()
+		subirArchivosDataLake("contenedorequipos", "escudos", ruta_imagenes)
 
-		con.cerrarConexion()
+	except Exception as e:
 
-		return "entorno_data_lake_creado"
+			mensaje=f"Motivo: {e}"
 
-	except Exception:
+			print(f"Error al subir los escudos al data lake")
 
-		return "log_data_lake"
+			crearArchivoLog(mensaje)
 
-def entorno_data_lake_creado():
-
-	if not entorno_creado("contenedorequipos"):
-
-		return "crear_entorno_data_lake"
-
-	return "subir_data_lake"
-
-def creacion_entorno_data_lake()->None:
-
-	crearEntornoDataLake("contenedorequipos", "escudos")
-
-	print("Entorno Data Lake creado")
-
-def subirEscudosDataLake()->None:
-
-	ruta_imagenes=os.path.join(os.getcwd(), "dags", "entorno", "imagenes", "escudos")
-
-	subirArchivosDataLake("contenedorequipos", "escudos", ruta_imagenes)
+	vaciarCarpeta(ruta_imagenes)
 
 
 with DAG("dag_futbol",
-		start_date=datetime(2024,6,8),
+		start_date=datetime(2024,6,12),
 		description="DAG para obtener datos de la web de futbol",
 		schedule_interval=None,
 		catchup=False) as dag:
 
-	tarea_existe_entorno=BranchPythonOperator(task_id="existe_entorno", python_callable=existe_entorno)
+	with TaskGroup("entorno") as tareas_entorno:
 
-	comando_bash_logs="cd ../../opt/airflow/dags && mkdir -p entorno/logs"
+		tarea_existe_entorno=BranchPythonOperator(task_id="existe_entorno", python_callable=existe_entorno)
 
-	comando_bash_imagenes="cd ../../opt/airflow/dags && mkdir -p entorno/imagenes/escudos"
+		comando_bash_logs="cd ../../opt/airflow/dags && mkdir -p entorno/logs"
 
-	tarea_carpeta_logs=BashOperator(task_id="carpeta_logs", bash_command=comando_bash_logs)
+		comando_bash_imagenes="cd ../../opt/airflow/dags && mkdir -p entorno/imagenes/escudos"
 
-	tarea_carpeta_imagenes=BashOperator(task_id="carpeta_imagenes", bash_command=comando_bash_imagenes)
+		tarea_carpeta_logs=BashOperator(task_id="carpeta_logs", bash_command=comando_bash_logs)
+
+		tarea_carpeta_imagenes=BashOperator(task_id="carpeta_imagenes", bash_command=comando_bash_imagenes)
+
+		tarea_entorno_creado=DummyOperator(task_id="entorno_creado")
+
+		tarea_existe_entorno >> [tarea_carpeta_logs, tarea_entorno_creado]
+
+		tarea_carpeta_logs >> tarea_carpeta_imagenes
+
+	with TaskGroup("pipelines_equipos") as tareas_pipelines_equipos:
+
+		tarea_pipeline_detalle_equipos=PythonOperator(task_id="pipeline_detalle_equipos", python_callable=Pipeline_Detalle_Equipos)
+
+		tarea_pipeline_escudo_equipos=PythonOperator(task_id="pipeline_escudo_equipos", python_callable=Pipeline_Escudo_Equipos)
+
+		tarea_pipeline_entrenador_equipos=PythonOperator(task_id="pipeline_entrenador_equipos", python_callable=Pipeline_Entrenador_Equipos)
+
+		tarea_pipeline_estadio_equipos=PythonOperator(task_id="pipeline_estadio_equipos", python_callable=Pipeline_Estadio_Equipos)
+
+	with TaskGroup("datalake") as tareas_datalake:
+
+		tarea_entorno_data_lake_creado=BranchPythonOperator(task_id="entorno_data_lake_creado", python_callable=entorno_data_lake_creado)
+
+		tarea_crear_entorno_data_lake=PythonOperator(task_id="crear_entorno_data_lake", python_callable=creacion_entorno_data_lake)
+
+		tarea_no_crear_entorno_data_lake=DummyOperator(task_id="no_crear_entorno_data_lake")
+
+		tarea_entorno_data_lake_creado >> [tarea_crear_entorno_data_lake, tarea_no_crear_entorno_data_lake]
 
 	tarea_pipeline_equipos_ligas=PythonOperator(task_id="pipeline_equipos_ligas", python_callable=Pipeline_Equipos_Ligas, trigger_rule="none_failed_min_one_success")
-
-	tarea_pipeline_detalle_equipos=PythonOperator(task_id="pipeline_detalle_equipos", python_callable=Pipeline_Detalle_Equipos)
-
-	tarea_pipeline_escudo_equipos=PythonOperator(task_id="pipeline_escudo_equipos", python_callable=Pipeline_Escudo_Equipos)
-
-	tarea_pipeline_entrenador_equipos=PythonOperator(task_id="pipeline_entrenador_equipos", python_callable=Pipeline_Entrenador_Equipos)
-
-	tarea_pipeline_estadio_equipos=PythonOperator(task_id="pipeline_estadio_equipos", python_callable=Pipeline_Estadio_Equipos)
-
-	tarea_descargar_escudos=PythonOperator(task_id="descargar_escudos", python_callable=descargarEscudos)
 
 	tarea_data_lake_disponible=BranchPythonOperator(task_id="data_lake_disponible", python_callable=data_lake_disponible)
 
 	tarea_log_data_lake=PythonOperator(task_id="log_data_lake", python_callable=crearArchivoLog, op_kwargs={"motivo": "Error en la conexion con el Data Lake"})
+	
+	tarea_subir_escudos_data_lake=PythonOperator(task_id="subir_escudos_data_lake", python_callable=subirEscudosDataLake, trigger_rule="none_failed_min_one_success")
 
-	tarea_entorno_data_lake_creado=BranchPythonOperator(task_id="entorno_data_lake_creado", python_callable=entorno_data_lake_creado)
 
-	tarea_crear_entorno_data_lake=PythonOperator(task_id="crear_entorno_data_lake", python_callable=creacion_entorno_data_lake)
+tareas_entorno >> tarea_pipeline_equipos_ligas >> tareas_pipelines_equipos >> tarea_data_lake_disponible >> [tareas_datalake, tarea_log_data_lake]
 
-	tarea_subir_data_lake=PythonOperator(task_id="subir_data_lake", python_callable=subirEscudosDataLake, trigger_rule="none_failed_min_one_success")
-
-tarea_existe_entorno >> [tarea_carpeta_logs, tarea_pipeline_equipos_ligas]
-
-tarea_carpeta_logs >> tarea_carpeta_imagenes >> tarea_pipeline_equipos_ligas
-
-tarea_pipeline_equipos_ligas >> tarea_pipeline_detalle_equipos >> tarea_pipeline_escudo_equipos >> tarea_pipeline_entrenador_equipos >> tarea_pipeline_estadio_equipos
-
-tarea_pipeline_estadio_equipos >> tarea_descargar_escudos >> tarea_data_lake_disponible >> [tarea_entorno_data_lake_creado, tarea_log_data_lake]
-
-tarea_entorno_data_lake_creado >> [tarea_crear_entorno_data_lake, tarea_subir_data_lake]
-
-tarea_crear_entorno_data_lake >> tarea_subir_data_lake
+tareas_datalake >> tarea_subir_escudos_data_lake
