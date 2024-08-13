66cfd5b7a7eb76746bcf830b087a135a90c650b5
nacho dorado <natxo98@gmail.com>
2024-08-13 23:30:55 +0200

    Creacion de funcion para limpiar los campeones de las competiciones

--- dags/python/src/etl_competicion_campeones.py
+++ dags/python/src/etl_competicion_campeones.py
@@ -8,3 +8,21 @@ def extraerDataCampeonesCompeticion(competicion:str)->Optional[pd.DataFrame]:
 	scraper=ScraperCompeticionCampeones(competicion)
 
 	return scraper.obtenerCampeonesCompeticion()
+
+def limpiarDataCampeonesCompeticion(tabla:pd.DataFrame)->pd.DataFrame:
+
+	tabla_filtrada=tabla[~(tabla["Ano"].isna())|tabla["Equipo_URL"].isna()]
+
+	if tabla_filtrada.empty:
+
+		raise Exception("No hay campeones disponibles")
+
+	tabla_filtrada=tabla_filtrada.reset_index(drop=True)
+
+	tabla_filtrada["Temporada"]=tabla_filtrada["Ano"].apply(lambda ano: int(ano))
+
+	tabla_filtrada["Equipo"]=tabla_filtrada["Equipo_URL"].apply(lambda url: url.split("/")[-1].strip())
+
+	columnas=["Temporada", "Equipo"]
+
+	return tabla_filtrada[columnas]
--- dags/python/tests/testetl/test_etl_competicion_campeones.py
+++ dags/python/tests/testetl/test_etl_competicion_campeones.py
@@ -1,7 +1,7 @@
 import pytest
 import pandas as pd
 
-from src.etl_competicion_campeones import extraerDataCampeonesCompeticion
+from src.etl_competicion_campeones import extraerDataCampeonesCompeticion, limpiarDataCampeonesCompeticion
 from src.scrapers.excepciones_scrapers import CompeticionCampeonesError
 
 @pytest.mark.parametrize(["endpoint"],
@@ -23,3 +23,37 @@ def test_extraer_data_competicion_campeones(competicion):
 
 	assert isinstance(data, pd.DataFrame)
 	assert not data.empty
+
+def test_limpiar_data_competicion_campeones_anos_nulos():
+
+	data=extraerDataCampeonesCompeticion("primera")
+
+	data["Ano"]=None
+
+	with pytest.raises(Exception):
+
+		limpiarDataCampeonesCompeticion(data)
+
+def test_limpiar_data_competicion_campeones_equipos_nulos():
+
+	data=extraerDataCampeonesCompeticion("primera")
+
+	data["Equipo_URL"]=None
+
+	with pytest.raises(Exception):
+
+		limpiarDataCampeonesCompeticion(data)
+
+@pytest.mark.parametrize(["competicion"],
+	[("primera",),("segunda",),("premier",),("serie_a",),("escocia",),
+	("primera_division_argentina",),("primera_division_rfef",),("champions",)]
+)
+def test_limpiar_data_competicion_campeones(competicion):
+
+	data=extraerDataCampeonesCompeticion(competicion)
+
+	data_limpia=limpiarDataCampeonesCompeticion(data)
+
+	assert isinstance(data_limpia, pd.DataFrame)
+	assert not data_limpia.empty
+	assert len(data_limpia.columns)==2
