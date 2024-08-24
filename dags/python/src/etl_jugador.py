58deb4e9c70a059225c98fb2506240b9f613c919
nacho dorado <natxo98@gmail.com>
2024-08-24 17:45:54 +0200

    Creacion de funcion para limpiar los datos de un jugador

--- dags/python/src/etl_jugador.py
+++ dags/python/src/etl_jugador.py
@@ -3,8 +3,30 @@ from typing import Optional
 
 from .scrapers.scraper_jugador import ScraperJugador
 
+from .utils import limpiarCodigoImagen
+
 def extraerDataJugador(jugador:str)->Optional[pd.DataFrame]:
 
 	scraper=ScraperJugador(jugador)
 
 	return scraper.obtenerJugador()
+
+def limpiarDataJugador(tabla:pd.DataFrame)->pd.DataFrame:
+
+	tabla["Nombre"]=tabla["Nombre"].apply(lambda nombre: nombre.strip() if nombre!="" else None)
+
+	tabla["Codigo_Equipo"]=tabla["Equipo_URL"].apply(limpiarCodigoImagen)
+
+	tabla["Codigo_Pais"]=tabla["Pais_URL"].apply(limpiarCodigoImagen)
+
+	tabla["Codigo_Jugador"]=tabla["Cara_URL"].apply(limpiarCodigoImagen)
+
+	tabla["Puntuacion"]=tabla["Puntuacion"].apply(lambda puntuacion: int(puntuacion) if puntuacion!="" else None)
+
+	tabla["Valor"]=tabla["Valor"].apply(lambda valor: float(valor) if valor!="" else None)
+
+	tabla["Dorsal"]=tabla["Dorsal"].apply(lambda dorsal: int(dorsal) if dorsal!="" else None)
+
+	columnas=["Nombre", "Codigo_Equipo", "Codigo_Pais", "Codigo_Jugador", "Puntuacion", "Valor", "Dorsal", "Posicion"]
+
+	return tabla[columnas]
--- dags/python/tests/testetl/test_etl_jugador.py
+++ dags/python/tests/testetl/test_etl_jugador.py
@@ -1,7 +1,7 @@
 import pytest
 import pandas as pd
 
-from src.etl_jugador import extraerDataJugador
+from src.etl_jugador import extraerDataJugador, limpiarDataJugador
 from src.scrapers.excepciones_scrapers import JugadorError
 
 @pytest.mark.parametrize(["endpoint"],
@@ -24,3 +24,18 @@ def test_extraer_data_jugador(jugador):
 	assert isinstance(data, pd.DataFrame)
 	assert not data.empty
 	assert len(data)==1
+
+@pytest.mark.parametrize(["jugador"],
+	[("j-alvarez-772644",),("f-torres-29366",),("d-villa-23386",),("c-gallagher-367792",),
+	("sorloth-232186",),("c-martin-776234",),("a-griezmann-32465",)]
+)
+def test_limpiar_data_jugador(jugador):
+
+	data=extraerDataJugador(jugador)
+
+	data_limpia=limpiarDataJugador(data)
+
+	assert isinstance(data_limpia, pd.DataFrame)
+	assert not data_limpia.empty
+	assert len(data_limpia.columns)==8
+	assert len(data_limpia)==1
