ce53559975d90c74e297ad1ea0c5bef370a2a92b
nacho dorado <natxo98@gmail.com>
2024-11-07 22:58:11 +0100

    Creacion de funcion para limpiar los datos de un estadio

--- dags/python/src/etl_estadio.py
+++ dags/python/src/etl_estadio.py
@@ -3,8 +3,20 @@ from typing import Optional
 
 from .scrapers.scraper_estadio import ScraperEstadio
 
+from .utils import limpiarCodigoImagen
+
 def extraerDataEstadio(estadio:str)->Optional[pd.DataFrame]:
 
 	scraper=ScraperEstadio(estadio)
 
 	return scraper.obtenerEstadio()
+
+def limpiarDataEstadio(tabla:pd.DataFrame)->pd.DataFrame:
+
+	tabla["Codigo_Estadio"]=tabla["Codigo_Estadio"].apply(limpiarCodigoImagen)
+
+	tabla["Codigo_Pais"]=tabla["Pais_URL"].apply(limpiarCodigoImagen)
+
+	columnas=["Pais", "Codigo_Pais"]
+
+	return tabla[columnas]
--- dags/python/tests/testetl/test_etl_estadio.py
+++ dags/python/tests/testetl/test_etl_estadio.py
@@ -1,7 +1,7 @@
 import pytest
 import pandas as pd
 
-from src.etl_estadio import extraerDataEstadio
+from src.etl_estadio import extraerDataEstadio, limpiarDataEstadio
 from src.scrapers.excepciones_scrapers import EstadioError
 
 @pytest.mark.parametrize(["endpoint"],
@@ -23,3 +23,17 @@ def test_extraer_data_estadio(estadio):
 	assert isinstance(data, pd.DataFrame)
 	assert not data.empty
 	assert len(data)==1
+
+@pytest.mark.parametrize(["estadio"],
+	[("riyadh-air-metropolitano-23",),("municipal-football-santa-amalia-4902",),("celtic-park-82",),("stadion-feijenoord-71",)]
+)
+def test_limpiar_data_estadio(estadio):
+
+	data=extraerDataEstadio(estadio)
+
+	data_limpia=limpiarDataEstadio(data)
+
+	assert isinstance(data_limpia, pd.DataFrame)
+	assert not data_limpia.empty
+	assert len(data_limpia.columns)==2
+	assert len(data_limpia)==1
