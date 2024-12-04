4ba95cf965951054a89b44d8a38593750b229e58
nacho dorado <natxo98@gmail.com>
2024-12-04 12:37:12 +0100

    Creacion de extraccion de los proximos partidos de un equipo. Creacion de funcion para limpiar los proximos partidos de un equipo

---
+++ dags/python/tests/testetl/test_etl_proximos_partidos.py
@@ -0,0 +1,50 @@
+import pytest
+import pandas as pd
+
+from src.etl_proximos_partidos import extraerDataProximosPartidosEquipo, limpiarDataProximosPartidosEquipo
+from src.scrapers.excepciones_scrapers import PartidosEquipoError
+
+@pytest.mark.parametrize(["equipo_id", "temporada"],
+	[(-1, -1), (0, 0), (0, 2019), (1, 2024), ("equipo", 2023)]
+)
+def test_extraer_data_proximos_partidos_equipo_error_endpoint(equipo_id, temporada):
+
+	with pytest.raises(PartidosEquipoError):
+
+		extraerDataProximosPartidosEquipo(equipo_id, temporada)
+
+@pytest.mark.parametrize(["equipo_id", "temporada"],
+	[(369, 2021),(369, 2014),(4, 2020),(449, 2017),(429, 1990),(369, 2000),(369, 1940),(449, 1971)]
+)
+def test_extraer_data_proximos_partidos_equipo(equipo_id, temporada):
+
+	data=extraerDataProximosPartidosEquipo(equipo_id, temporada)
+
+	assert isinstance(data, pd.DataFrame)
+	assert not data.empty
+
+@pytest.mark.parametrize(["equipo_id", "temporada"],
+	[(369, 2021),(369, 2014),(4, 2020),(449, 2017),(429, 1990),(369, 2000),(369, 1940),(449, 1971),(2115, 2024)]
+)
+def test_limpiar_data_proximos_partidos_equipo_no_hay(equipo_id, temporada):
+
+	data=extraerDataProximosPartidosEquipo(equipo_id, temporada)
+
+	with pytest.raises(Exception):
+
+		limpiarDataProximosPartidosEquipo(data)
+
+@pytest.mark.parametrize(["equipo_id", "temporada"],
+	[(369, 2021),(369, 2014),(4, 2020),(449, 2017),(429, 1990),(369, 2000),(369, 1940),(449, 1971),(2115, 2024)]
+)
+def test_limpiar_data_proximos_partidos_equipo(equipo_id, temporada):
+
+	data=extraerDataProximosPartidosEquipo(equipo_id, temporada)
+
+	data["Estado"]=-1
+
+	data_limpia=limpiarDataProximosPartidosEquipo(data)
+
+	assert isinstance(data_limpia, pd.DataFrame)
+	assert not data_limpia.empty
+	assert len(data_limpia.columns)==6
