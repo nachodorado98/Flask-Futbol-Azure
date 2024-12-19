6bfff7c12ab6e95f91f2c348fb0addae1d5daa04
nacho dorado <natxo98@gmail.com>
2024-12-19 14:16:16 +0100

    Creacion de funcion para limpiar los datos de un entrenador

--- dags/python/src/etl_entrenador.py
+++ dags/python/src/etl_entrenador.py
@@ -3,8 +3,26 @@ from typing import Optional
 
 from .scrapers.scraper_entrenador import ScraperEntrenador
 
+from .utils import limpiarCodigoImagen
+
 def extraerDataEntrenador(entrenador:str)->Optional[pd.DataFrame]:
 
 	scraper=ScraperEntrenador(entrenador)
 
 	return scraper.obtenerEntrenador()
+
+def limpiarDataEntrenador(tabla:pd.DataFrame)->pd.DataFrame:
+
+	tabla["Nombre"]=tabla["Nombre"].apply(lambda nombre: nombre.strip() if nombre!="" else None)
+
+	tabla["Codigo_Equipo"]=tabla["Equipo_URL"].apply(limpiarCodigoImagen)
+
+	tabla["Codigo_Pais"]=tabla["Pais_URL"].apply(limpiarCodigoImagen)
+
+	tabla["Codigo_Entrenador"]=tabla["Cara_URL"].apply(limpiarCodigoImagen)
+
+	tabla["Puntuacion"]=tabla["Puntuacion"].apply(lambda puntuacion: int(puntuacion) if puntuacion!="" else None)
+
+	columnas=["Nombre", "Codigo_Equipo", "Codigo_Pais", "Codigo_Entrenador", "Puntuacion"]
+	
+	return tabla[columnas]
--- dags/python/tests/testetl/test_etl_entrenador.py
+++ dags/python/tests/testetl/test_etl_entrenador.py
@@ -1,7 +1,7 @@
 import pytest
 import pandas as pd
 
-from src.etl_entrenador import extraerDataEntrenador
+from src.etl_entrenador import extraerDataEntrenador, limpiarDataEntrenador
 from src.scrapers.excepciones_scrapers import EntrenadorError
 
 @pytest.mark.parametrize(["endpoint"],
@@ -24,3 +24,18 @@ def test_extraer_data_entrenador(entrenador):
 	assert isinstance(data, pd.DataFrame)
 	assert not data.empty
 	assert len(data)==1
+
+@pytest.mark.parametrize(["entrenador"],
+	[("diego-simeone-13",),("hansi-flick-8143",),("thiago-motta-21853",),
+	("fernando-torres-47437",),("luis-aragones-1918",),("radomir-antic-2601",)]
+)
+def test_limpiar_data_entrenador(entrenador):
+
+	data=extraerDataEntrenador(entrenador)
+
+	data_limpia=limpiarDataEntrenador(data)
+
+	assert isinstance(data_limpia, pd.DataFrame)
+	assert not data_limpia.empty
+	assert len(data_limpia.columns)==5
+	assert len(data_limpia)==1
