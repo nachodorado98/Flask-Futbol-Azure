b173b34dfc364e1f62e4238ab4c8b0c93fa41430
nacho dorado <natxo98@gmail.com>
2025-01-06 20:19:35 +0100

    Creacion de funcion para limpiar los datos de los equipos de un jugador

--- dags/python/src/etl_jugador_equipos.py
+++ dags/python/src/etl_jugador_equipos.py
@@ -3,8 +3,22 @@ from typing import Optional
 
 from .scrapers.scraper_jugador_equipos import ScraperJugadorEquipos
 
+from .utils import limpiarCodigoImagen
+
 def extraerDataJugadorEquipos(jugador:str)->Optional[pd.DataFrame]:
 
 	scraper=ScraperJugadorEquipos(jugador)
 
 	return scraper.obtenerJugadorEquipos()
+
+def limpiarDataJugadorEquipos(tabla:pd.DataFrame)->pd.DataFrame:
+
+	tabla["Codigo_Equipo"]=tabla["Equipo_URL"].apply(limpiarCodigoImagen)
+
+	tabla["Temporadas"]=tabla["Temporadas"].apply(lambda temporada: int(temporada.split(" ")[0]))
+
+	tabla=tabla.astype({"Goles": int, "Partidos": int})
+
+	columnas=["Codigo_Equipo", "Temporadas", "Goles", "Partidos"]
+
+	return tabla[columnas]
--- dags/python/tests/testetl/test_etl_jugador_equipos.py
+++ dags/python/tests/testetl/test_etl_jugador_equipos.py
@@ -1,7 +1,7 @@
 import pytest
 import pandas as pd
 
-from src.etl_jugador_equipos import extraerDataJugadorEquipos
+from src.etl_jugador_equipos import extraerDataJugadorEquipos, limpiarDataJugadorEquipos
 from src.scrapers.excepciones_scrapers import JugadorEquiposError
 
 @pytest.mark.parametrize(["endpoint"],
@@ -24,3 +24,17 @@ def test_extraer_data_jugador_equipos(jugador):
 	assert isinstance(data, pd.DataFrame)
 	assert not data.empty
 	assert len(data.columns)==4
+
+@pytest.mark.parametrize(["jugador"],
+	[("j-alvarez-772644",),("f-torres-29366",),("d-villa-23386",),("c-gallagher-367792",),
+	("sorloth-232186",),("c-martin-776234",),("a-griezmann-32465",)]
+)
+def test_limpiar_data_jugador_equipos(jugador):
+
+	data=extraerDataJugadorEquipos(jugador)
+
+	data_limpia=limpiarDataJugadorEquipos(data)
+
+	assert isinstance(data_limpia, pd.DataFrame)
+	assert not data_limpia.empty
+	assert len(data_limpia.columns)==4
