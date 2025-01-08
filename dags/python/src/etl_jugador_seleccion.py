929a0da46e6ded58569afb1255c96bc6bd60f3f4
nacho dorado <natxo98@gmail.com>
2025-01-09 00:34:23 +0100

    Creacion de funcion para limpiar los datos de la seleccion de un jugador

--- dags/python/src/etl_jugador_seleccion.py
+++ dags/python/src/etl_jugador_seleccion.py
@@ -3,8 +3,30 @@ from typing import Optional
 
 from .scrapers.scraper_jugador_seleccion import ScraperJugadorSeleccion
 
+from .utils import limpiarCodigoImagen
+
 def extraerDataJugadorSeleccion(jugador:str)->Optional[pd.DataFrame]:
 
 	scraper=ScraperJugadorSeleccion(jugador)
 
 	return scraper.obtenerJugadorSeleccion()
+
+def limpiarDataJugadorSeleccion(tabla:pd.DataFrame)->pd.DataFrame:
+
+	tabla["Codigo_Seleccion"]=tabla["Codigo_Seleccion"].apply(limpiarCodigoImagen)
+
+	tabla["Convocatorias"]=tabla["Convocatorias"].apply(lambda texto: int(texto.split(" veces")[0]))
+
+	tabla["Goles"]=tabla["Goles"].apply(lambda goles: 0 if goles=="-" else int(goles))
+
+	tabla["Media"]=tabla["Media"].apply(lambda media: 0.0 if media=="-" else float(media))
+
+	tabla["Asistencias"]=tabla["Asistencias"].apply(lambda asistencias: 0 if asistencias=="-" else int(asistencias))
+
+	tabla["Amarillas"]=tabla["Amarillas"].apply(lambda amarillas: 0 if amarillas=="-" else int(amarillas))
+
+	tabla["Rojas"]=tabla["Rojas"].apply(lambda rojas: 0 if rojas=="-" else int(rojas))
+
+	columnas=["Codigo_Seleccion", "Convocatorias", "Goles", "Asistencias"]
+
+	return tabla[columnas]
--- dags/python/tests/testetl/test_etl_jugador_seleccion.py
+++ dags/python/tests/testetl/test_etl_jugador_seleccion.py
@@ -1,7 +1,7 @@
 import pytest
 import pandas as pd
 
-from src.etl_jugador_seleccion import extraerDataJugadorSeleccion
+from src.etl_jugador_seleccion import extraerDataJugadorSeleccion, limpiarDataJugadorSeleccion
 from src.scrapers.excepciones_scrapers import JugadorSeleccionError
 
 @pytest.mark.parametrize(["endpoint"],
@@ -34,3 +34,19 @@ def test_extraer_data_jugador_seleccion(jugador):
 	assert isinstance(data, pd.DataFrame)
 	assert not data.empty
 	assert len(data.columns)==7
+	assert len(data)==1
+
+@pytest.mark.parametrize(["jugador"],
+	[("j-alvarez-772644",),("f-torres-29366",),("d-villa-23386",),("c-gallagher-367792",),
+	("sorloth-232186",),("c-martin-776234",),("a-griezmann-32465",)]
+)
+def test_limpiar_data_jugador_seleccion(jugador):
+
+	data=extraerDataJugadorSeleccion(jugador)
+
+	data_limpia=limpiarDataJugadorSeleccion(data)
+
+	assert isinstance(data_limpia, pd.DataFrame)
+	assert not data_limpia.empty
+	assert len(data_limpia.columns)==4
+	assert len(data_limpia)==1
