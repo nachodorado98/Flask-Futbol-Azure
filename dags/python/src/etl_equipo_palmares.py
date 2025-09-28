094e0718124685aa0ec78a6593531b75904e24b4
nacho dorado <natxo98@gmail.com>
2025-09-28 21:08:40 +0200

    Creacion de funcion para limpiar los datos del palmares de un equipo

--- dags/python/src/etl_equipo_palmares.py
+++ dags/python/src/etl_equipo_palmares.py
@@ -3,8 +3,22 @@ from typing import Optional
 
 from .scrapers.scraper_equipo_palmares import ScraperEquipoPalmares
 
+from .utils import limpiarCodigoImagen
+
 def extraerDataEquipoPalmares(equipo:str)->Optional[pd.DataFrame]:
 
 	scraper=ScraperEquipoPalmares(equipo)
 
 	return scraper.obtenerEstadioPalmares()
+
+def limpiarDataEquipoPalmares(tabla:pd.DataFrame)->pd.DataFrame:
+
+	tabla["Imagen_Titulo"]=tabla["Imagen_Titulo"].apply(limpiarCodigoImagen)
+
+	tabla["Imagen_Competicion"]=tabla["Imagen_Competicion"].apply(limpiarCodigoImagen)
+
+	tabla["Numero"]=tabla["Numero"].apply(lambda numero: int(numero))
+
+	columnas=["Nombre", "Numero", "Imagen_Competicion", "Imagen_Titulo", "Annos"]
+
+	return tabla[columnas]
--- dags/python/tests/testetl/test_etl_equipo_palmares.py
+++ dags/python/tests/testetl/test_etl_equipo_palmares.py
@@ -1,7 +1,7 @@
 import pytest
 import pandas as pd
 
-from src.etl_equipo_palmares import extraerDataEquipoPalmares
+from src.etl_equipo_palmares import extraerDataEquipoPalmares, limpiarDataEquipoPalmares
 from src.scrapers.excepciones_scrapers import EquipoPalmaresError
 
 @pytest.mark.parametrize(["endpoint"],
@@ -32,3 +32,17 @@ def test_extraer_data_equipo_palmares(equipo):
 	assert isinstance(data, pd.DataFrame)
 	assert not data.empty
 	assert len(data)>1
+
+@pytest.mark.parametrize(["equipo"],
+	[("atletico-madrid",),("liverpool",),("barcelona",),("sporting-gijon",)]
+)
+def test_limpiar_data_equipo_palmares(equipo):
+
+	data=extraerDataEquipoPalmares(equipo)
+
+	data_limpia=limpiarDataEquipoPalmares(data)
+
+	assert isinstance(data_limpia, pd.DataFrame)
+	assert not data.empty
+	assert len(data)==len(data_limpia)
+	assert len(data_limpia.columns)==5
