d7cefe7da8817691b50f27e9562da74411db4b9b
nacho dorado <natxo98@gmail.com>
2025-11-15 00:53:17 +0100

    Creacion de funcion para limpiar los datos de los equipos de los entrenadores

--- dags/python/src/etl_entrenador_equipos.py
+++ dags/python/src/etl_entrenador_equipos.py
@@ -1,10 +1,38 @@
 import pandas as pd
 from typing import Optional
+from datetime import datetime
 
 from .scrapers.scraper_entrenador_equipos import ScraperEntrenadorEquipos
+
+from .utils import limpiarCodigoImagen
 
 def extraerDataEntrenadorEquipos(entrenador:str)->Optional[pd.DataFrame]:
 
 	scraper=ScraperEntrenadorEquipos(entrenador)
 
 	return scraper.obtenerEntrenadorEquipos()
+
+def limpiarDataEntrenadorEquipos(tabla:pd.DataFrame)->pd.DataFrame:
+
+	
+
+	tabla["Codigo_Equipo"]=tabla["Equipo_URL"].apply(limpiarCodigoImagen)
+
+	tabla["Desde"]=tabla["Desde"].apply(lambda fecha: datetime.strptime(fecha, "%d-%m-%Y").strftime("%Y-%m-%d"))
+
+	tabla["Hasta"]=tabla["Hasta"].apply(lambda fecha: datetime.strptime(fecha, "%d-%m-%Y").strftime("%Y-%m-%d"))
+
+	tabla["_Desde_dt"]=pd.to_datetime(tabla["Desde"])
+
+	tabla=tabla.sort_values("_Desde_dt")
+
+	tabla=tabla.astype({"Partidos_Totales": int, "Ganados": int, "Empatados": int, "Perdidos": int})
+
+	tabla["Duracion"]=tabla["Desde"]+","+tabla["Hasta"]
+
+	tabla=tabla.groupby("Codigo_Equipo").agg({"Partidos_Totales": "sum", "Ganados": "sum", "Empatados": "sum", "Perdidos": "sum",
+        										"Duracion": lambda fechas: ";".join(fechas), "Tactica": "first"}).reset_index()
+
+	columnas=["Codigo_Equipo", "Partidos_Totales", "Duracion", "Ganados", "Empatados", "Perdidos", "Tactica"]
+
+	return tabla[columnas]
--- dags/python/tests/testetl/test_etl_entrenador_equipos.py
+++ dags/python/tests/testetl/test_etl_entrenador_equipos.py
@@ -1,7 +1,7 @@
 import pytest
 import pandas as pd
 
-from src.etl_entrenador_equipos import extraerDataEntrenadorEquipos
+from src.etl_entrenador_equipos import extraerDataEntrenadorEquipos, limpiarDataEntrenadorEquipos
 from src.scrapers.excepciones_scrapers import EntrenadorEquiposError
 
 @pytest.mark.parametrize(["endpoint"],
@@ -23,3 +23,16 @@ def test_extraer_data_entrenador_equipos(entrenador):
 	assert isinstance(data, pd.DataFrame)
 	assert not data.empty
 	assert len(data.columns)==8
+
+@pytest.mark.parametrize(["entrenador"],
+	[("diego-simeone-13",),("hansi-flick-8143",),("thiago-motta-21853",),("fernando-torres-47437",)]
+)
+def test_limpiar_data_entrenador_equipos(entrenador):
+
+	data=extraerDataEntrenadorEquipos(entrenador)
+
+	data_limpia=limpiarDataEntrenadorEquipos(data)
+
+	assert isinstance(data_limpia, pd.DataFrame)
+	assert not data_limpia.empty
+	assert len(data_limpia.columns)==7
